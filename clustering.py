408507c496c5dc2021871e75574cd71c4820105d
ElaineYChu <elainechu@nevada.unr.edu>
2019-10-17 22:16:16 -0700

    made some extra tweaks to clustering.py

--- clustering.py
+++ clustering.py
@@ -2,14 +2,6 @@ import numpy as np
 import random
 import statistics as stat
 
-X = np.array([[0],[1],[2],[7],[8],[9],[12],[14],[15]])
-K = 3
-
-X2 = np.array([[1, 0], [7, 4], [9, 6], [2, 1], [4, 8], [0, 3], [13, 5], [6, 8], [7, 3], [3, 6], [2, 1], [8, 3], [10, 2], [3, 5], [5, 1], [1, 9], [10, 3], [4, 1], [6, 6], [2, 2]])
-
-
-
-
 def K_Means(X, K):
     num_samps = X.shape[0]  # store number of samples
     
@@ -20,7 +12,7 @@ def K_Means(X, K):
     old_clusters = [0] * K
     
     for a in range(K):
-        clusters.append(list(X[cluster_index[a]])) # random initial clusters
+        clusters.append(tuple(X[cluster_index[a]])) # random initial clusters
      
     for i in range(999): 
         if old_clusters == clusters:  # check if clusters stayed the same
@@ -28,6 +20,7 @@ def K_Means(X, K):
         else:
             old_clusters = clusters  # if not, old cluster is rewritten
             clusters = []  # clear clusters list
+            newlabels = []  # clear newlabels list
             for b in range(num_samps):  # loop through samples
                 samp = X[b]  # select sample
                 centroid_dist = []
@@ -47,12 +40,12 @@ def K_Means(X, K):
                 index_list = np.where(np.asarray(newlabels)==d)[0]  # find index of all samples closest to current centroid
                 if len(index_list) == 0:  # if centroid is alone
                     clusters.append(old_clusters[d])  # make it the old one to avoid nan
+                    clusters = sorted(clusters)  # sort cluster centers
                 else:   
                     new_mean = np.nanmean(X[index_list,], axis=0)  # calculate new mean using closest samples
                     clusters.append(tuple(new_mean))  # add to clusters list
-            newlabels = []  # clear newlabels list
+                    clusters = sorted(clusters)  # sort cluster centers
             
-    clusters = sorted(clusters)
     C = clusters # C = clusters to be uniform to project
     C = np.array(C)  # converts C to numpy array
     return C
